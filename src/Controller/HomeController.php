5ef4c0d48bbcae4d8ee775665da09fa38f7d3464
Flavien <thepokeradvisor@gmail.com>
2021-12-08 17:32:08 +0100

    Display all bars in home page + disabled edit delete if not granted

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EtablissementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(EtablissementRepository $etablissementRepository): Response
     {
+        $etablissements = $etablissementRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'etablissements' => $etablissements,
         ]);
     }
 }
